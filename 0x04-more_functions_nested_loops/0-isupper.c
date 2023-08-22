b3885320f1ff006abe3f64fd387181d5f4ac88e6
malusi123 <malusizwane321@gmail.com>
2023-08-22 11:48:28 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -27,12 +27,12 @@ int _isupper(int c)
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
 
